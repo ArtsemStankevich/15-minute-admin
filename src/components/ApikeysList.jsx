dec1e0ce66c50f42a04a584c328c9e3f2dffad5b
ArtsemStankevich <artsemstankevich@gmail.com>
2023-11-18 14:37:46 +0100

    Add auto-navigate to /login

--- src/components/ApikeysList.jsx
+++ src/components/ApikeysList.jsx
@@ -2,8 +2,11 @@ import React, { useState, useEffect } from 'react';
 import TableContainer from './TableContainer';
 import ApikeysCreate from './ApikeysCreate';
 import {Container} from 'reactstrap'
+import { useNavigate } from 'react-router-dom';
+
 
 function ApikeysList() {
+  const navigate = useNavigate();
   const [api, setApi] = useState([]);
 
   const columns = React.useMemo(
@@ -24,7 +27,28 @@ function ApikeysList() {
       const tokenString = localStorage.getItem('token');
       const userToken = JSON.parse(tokenString);
 
+
+      const tokenRefresh = {
+        refresh : userToken,
+      };
+
       if (userToken) {
+        const responseToken = await fetch('https://15minadmin.1213213.xyz/users//token/refresh/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${userToken}`, // Dodaj token do nagłówka Authorization
+          },
+          body: JSON.stringify(tokenRefresh)
+        });
+        console.log(responseToken)
+        if (responseToken.ok){
+          const data = await responseToken.json();
+          localStorage.setItem('token', JSON.stringify(data.access))
+        }else{
+          console.error("Błąd podczas refresh token")
+        }
+
         const response = await fetch('https://15minadmin.1213213.xyz/gmaps/credential/', {
           method: 'GET',
           headers: {
@@ -38,12 +62,15 @@ function ApikeysList() {
           setApi(data);
         } else {
           console.error('Błąd pobierania danych z serwera');
+          navigate('/login');
         }
       } else {
         console.error('Brak tokenu użytkownika.');
       }
     } catch (error) {
       console.error('Błąd pobierania danych z serwera', error);
+      navigate('/login');
+
     }
   };
 
