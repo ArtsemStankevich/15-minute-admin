367691e01399285e546d66f1e9d6a78a2ce64a41
ArtsemStankevich <artsemstankevich@gmail.com>
2023-11-18 15:16:04 +0100

    add auto navigate to login without any warnings

--- src/components/ApikeysList.jsx
+++ src/components/ApikeysList.jsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import TableContainer from './TableContainer';
 import ApikeysCreate from './ApikeysCreate';
-import {Container} from 'reactstrap'
+import { Container } from 'reactstrap';
 import { useNavigate } from 'react-router-dom';
-
 
 function ApikeysList() {
   const navigate = useNavigate();
@@ -21,34 +20,32 @@ function ApikeysList() {
     []
   );
 
-  const fetchApi = async () => {
+  const fetchApi = useCallback(async() => {
     try {
-
       const tokenString = localStorage.getItem('token');
       const userToken = JSON.parse(tokenString);
-
-
+/*
       const tokenRefresh = {
-        refresh : userToken,
+        refresh: userToken,
       };
-
+*/
       if (userToken) {
-        const responseToken = await fetch('https://15minadmin.1213213.xyz/users//token/refresh/', {
+/*        const responseToken = await fetch('https://15minadmin.1213213.xyz/users//token/refresh/', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${userToken}`, // Dodaj token do nagłówka Authorization
           },
-          body: JSON.stringify(tokenRefresh)
+          body: JSON.stringify(tokenRefresh),
         });
-        console.log(responseToken)
-        if (responseToken.ok){
+        console.log(responseToken);
+        if (responseToken.ok) {
           const data = await responseToken.json();
-          localStorage.setItem('token', JSON.stringify(data.access))
-        }else{
-          console.error("Błąd podczas refresh token")
+          localStorage.setItem('token', JSON.stringify(data.access));
+        } else {
+          console.error('Błąd podczas refresh token');
         }
-
+*/
         const response = await fetch('https://15minadmin.1213213.xyz/gmaps/credential/', {
           method: 'GET',
           headers: {
@@ -61,6 +58,7 @@ function ApikeysList() {
           const data = await response.json();
           setApi(data);
         } else {
+          console.log(response)
           console.error('Błąd pobierania danych z serwera');
           navigate('/login');
         }
@@ -70,13 +68,12 @@ function ApikeysList() {
     } catch (error) {
       console.error('Błąd pobierania danych z serwera', error);
       navigate('/login');
-
     }
-  };
+  }, [navigate])
 
   useEffect(() => {
     fetchApi();
-  }, []);
+  }, [fetchApi]); // Usuń fetchApi zależność, aby uniknąć cyklu zależności
 
   const handleApiCreated = () => {
     // Po utworzeniu klucza API odśwież listę
