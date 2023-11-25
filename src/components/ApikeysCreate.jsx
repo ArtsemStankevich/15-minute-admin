1c34f945b7db07cedd95b06f596aa70e178eb5f8
ArtsemStankevich <artsemstankevich@gmail.com>
2023-11-25 15:25:02 +0100

    add support for Api key token

--- src/components/ApikeysCreate.jsx
+++ src/components/ApikeysCreate.jsx
@@ -6,6 +6,7 @@ import './style/Categories.css';
 function ApikeysCreate({ onApiCreated }) {
   const [newNameApi, setNewNameApi] = useState('');
   const [newTokenApi, setNewTokenApi] = useState('');
+  const [error, setError] = useState('');
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
@@ -25,6 +26,13 @@ function ApikeysCreate({ onApiCreated }) {
       token: newTokenApi,
       name: newNameApi,
     };
+
+    if (newTokenApi.length !== 39 || !/^[A-Za-z0-9_-]+$/.test(newTokenApi)) {
+      setError('Token must be exactly 39 characters long and consist of letters, numbers, "_" and "-"');
+      return;
+    } else {
+      setError('');
+    }
 
     try {
 
@@ -83,6 +91,7 @@ function ApikeysCreate({ onApiCreated }) {
     <div>
       <p className='borderer'>
         <h3 className='auto-center'>New Api Key</h3>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
         <form onSubmit={handleFormSubmit}>
           <TextField
             label="Name"
@@ -103,7 +112,7 @@ function ApikeysCreate({ onApiCreated }) {
             onChange={(e) => setNewTokenApi(e.target.value)}
           />
           <Button variant="contained" color="primary" type="submit" style={{ margin: '1% auto 0', backgroundColor: 'darkblue', marginLeft: '30px' }}>
-              Add Api Key
+            Add Api Key
           </Button>
         </form>
       </p>
