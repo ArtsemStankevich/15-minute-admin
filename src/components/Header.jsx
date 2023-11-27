3e987321830ed58abd65bce4315bd448c7d37715
ArtsemStankevich <artsemstankevich@gmail.com>
2023-11-27 15:29:56 +0100

    button i18n done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,18 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import { useTranslation } from 'react-i18next'; // Import react-i18next
 import './style/Header.css';
 
 function Header() {
   const [selectedTab, setSelectedTab] = useState('tasks');
+  const { i18n, t } = useTranslation();
+  const [selectedLanguage, setSelectedLanguage] = useState(i18n.language);
+
   const navigate = useNavigate();
 
   const handleNavigation = (tab) => {
@@ -13,9 +21,25 @@ function Header() {
     navigate(`/${tab.toLowerCase()}`);
   };
 
+
+  const handleLanguageChange = (lng) => {
+    setSelectedLanguage(lng);
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <div>
       <div className="tab-buttons">
+        <Select
+          value={selectedLanguage}
+          onChange={e => handleLanguageChange(e.target.value)}
+          className='language-select'
+        >
+          <MenuItem value="en">English</MenuItem>
+          <MenuItem value="pl">Polski</MenuItem>
+          <MenuItem value="de">Niemiecki</MenuItem>
+          {/* Add more languages as needed */}
+        </Select>
         <ButtonGroup className="header-button-group" variant="contained" aria-label="outlined button group">
           <Button
             className={`header-button ${selectedTab === 'tasks' ? 'active' : ''}`}
