bc11bfbdaa809c66f467c57c4b07652203cf76f4
ArtsemStankevich <93472117+ArtsemStankevich@users.noreply.github.com>
2023-12-10 13:29:30 +0100

    uninstall eslint and test pre commit (#22)

--- src/components/CoordinatesCreate.jsx
+++ src/components/CoordinatesCreate.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import "./style/Categories.css";
-import { useTranslation } from "react-i18next";
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import './style/Categories.css';
+import { useTranslation } from 'react-i18next';
 
 function ApikeysCreate({ onCoordinateCreated }) {
-  const [newNameCoordinates, setnewNameCoordinates] = useState("");
-  const [newLon, setNewLon] = useState("");
-  const [newLat, setNewLat] = useState("");
-  const [newRadius, setNewRadius] = useState("");
-  const [error, setError] = useState("");
+  const [newNameCoordinates, setnewNameCoordinates] = useState('');
+  const [newLon, setNewLon] = useState('');
+  const [newLat, setNewLat] = useState('');
+  const [newRadius, setNewRadius] = useState('');
+  const [error, setError] = useState('');
   const { t } = useTranslation();
 
   const handleFormSubmit = async (e) => {
@@ -38,18 +38,18 @@ function ApikeysCreate({ onCoordinateCreated }) {
       lon > 180
     ) {
       setError(
-        "Invalid latitude or longitude values. Latitude should be in the range -90 to 90, and longitude should be in the range -180 to 180.",
+        'Invalid latitude or longitude values. Latitude should be in the range -90 to 90, and longitude should be in the range -180 to 180.',
       );
       return;
     } else {
-      setError("");
+      setError('');
     }
 
     try {
-      const tokenRefreshString = localStorage.getItem("refreshToken");
+      const tokenRefreshString = localStorage.getItem('refreshToken');
       const userRefreshToken = JSON.parse(tokenRefreshString);
 
-      const tokenString = localStorage.getItem("token");
+      const tokenString = localStorage.getItem('token');
       const userToken = JSON.parse(tokenString);
 
       const tokenRefresh = {
@@ -57,11 +57,11 @@ function ApikeysCreate({ onCoordinateCreated }) {
       };
 
       const responseToken = await fetch(
-        "https://15minadmin.1213213.xyz/users//token/refresh/",
+        'https://15minadmin.1213213.xyz/users//token/refresh/',
         {
-          method: "POST",
+          method: 'POST',
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
           },
           body: JSON.stringify(tokenRefresh),
         },
@@ -70,18 +70,18 @@ function ApikeysCreate({ onCoordinateCreated }) {
       console.log(responseToken);
       if (responseToken.ok) {
         const data = await responseToken.json();
-        localStorage.setItem("refreshToken", JSON.stringify(data.refresh));
-        localStorage.setItem("token", JSON.stringify(data.access));
+        localStorage.setItem('refreshToken', JSON.stringify(data.refresh));
+        localStorage.setItem('token', JSON.stringify(data.access));
       } else {
-        console.error("Błąd podczas refresh token");
+        console.error('Błąd podczas refresh token');
       }
 
       const response = await fetch(
-        "https://15minadmin.1213213.xyz/gmaps/coordinates/",
+        'https://15minadmin.1213213.xyz/gmaps/coordinates/',
         {
-          method: "POST",
+          method: 'POST',
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
             Authorization: `Bearer ${userToken}`, // Dodaj token do nagłówka Authorization
           },
           body: JSON.stringify(apiData), // Zamień dane na format JSON
@@ -90,54 +90,54 @@ function ApikeysCreate({ onCoordinateCreated }) {
 
       if (response.ok) {
         // Wysłano z powodzeniem
-        console.log("Pomyślnie utworzono klucz API.");
+        console.log('Pomyślnie utworzono klucz API.');
         // Możesz również zaktualizować stan lub zresetować pola formularza
         onCoordinateCreated();
-        setnewNameCoordinates("");
-        setNewLon("");
+        setnewNameCoordinates('');
+        setNewLon('');
       } else {
-        console.error("Błąd podczas tworzenia klucza API.");
+        console.error('Błąd podczas tworzenia klucza API.');
       }
     } catch (error) {
-      console.error("Błąd podczas komunikacji z serwerem", error);
+      console.error('Błąd podczas komunikacji z serwerem', error);
     }
   };
 
   return (
     <div>
       <p className="borderer">
-        <h3 className="auto-center">{t("New Coordinates")}</h3>
-        {error && <p style={{ color: "red" }}>{error}</p>}
+        <h3 className="auto-center">{t('New Coordinates')}</h3>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
         <form onSubmit={handleFormSubmit}>
           <TextField
-            label={t("Coordinates Name")}
+            label={t('Coordinates Name')}
             multiline
             rows={1}
             variant="outlined"
             value={newNameCoordinates}
             onChange={(e) => setnewNameCoordinates(e.target.value)}
-            style={{ marginRight: "20px" }}
+            style={{ marginRight: '20px' }}
           />
           <TextField
-            label={t("Latitude")}
+            label={t('Latitude')}
             multiline
             rows={1}
             variant="outlined"
             value={newLat}
             onChange={(e) => setNewLat(e.target.value)}
-            style={{ marginRight: "20px" }}
+            style={{ marginRight: '20px' }}
           />
           <TextField
-            label={t("Longitude")}
+            label={t('Longitude')}
             multiline
             rows={1}
             variant="outlined"
             value={newLon}
             onChange={(e) => setNewLon(e.target.value)}
-            style={{ marginRight: "20px" }}
+            style={{ marginRight: '20px' }}
           />
           <TextField
-            label={t("Radius")}
+            label={t('Radius')}
             multiline
             rows={1}
             variant="outlined"
@@ -149,9 +149,9 @@ function ApikeysCreate({ onCoordinateCreated }) {
             color="primary"
             type="submit"
             style={{
-              margin: "1% auto 0",
-              backgroundColor: "darkblue",
-              marginLeft: "30px",
+              margin: '1% auto 0',
+              backgroundColor: 'darkblue',
+              marginLeft: '30px',
             }}
           >
             Add Coordinate
